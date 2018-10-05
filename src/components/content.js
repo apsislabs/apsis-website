e96f48c472c3498d30a610e8763522dc408f62cd
Marni Cohen <marni.a.cohen@gmail.com>
2018-10-05 15:59:29 -0700

    add services list to homepage

--- src/components/content.js
+++ src/components/content.js
@@ -5,16 +5,16 @@ import buttonStyles from "./button.module.css"
 
 export default props => (
     <div className={contentStyles.content}>  
-      <h2>{props.title}</h2>
+        <h2>{props.title}</h2>
 
-      <div className={contentStyles.contentTextContainer}>
-      <div className={contentStyles.contentText}>{props.text}</div>
-      <div>{props.children}</div>
-        <Button
-            className={buttonStyles.herobutton}
-            to="/services"
-            text="Our Services"
-        />
-    </div>
+        <div className={contentStyles.contentTextContainer}>
+            <div className={contentStyles.contentText}>{props.text}</div>
+            <div className={contentStyles.contentChildren}>{props.children}</div>
+            <Button
+                className={buttonStyles.herobutton}
+                to="/services"
+                text="Our Services"
+            />
+        </div>
     </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import Hero from "../components/hero"
 import Clients from "../components/clients"
 import Content from "../components/content"
+import Service from "../components/service"
 
 const IndexPage = (props) => (
     <Layout>
@@ -23,7 +24,38 @@ const IndexPage = (props) => (
         <Content
             title="Your industry is our business."
             text="Our craft is software development but our specialty is understanding your business, your processes, and your priorities. A robust understanding of your business goals is crucial for us to determine how to serve you best. We take the time to ensure you’re not paying extra for something that’s over-engineered.  We want to strike the right balance among the factors that are important to you: effective features and functionality, budget, speed, technology maintenance costs, and more."
-        />
+        >
+            <Service
+                icon="far fa-compass"
+                title="Web Development"
+            >
+                <ul>
+                    <li>Ruby on Rails</li>
+                    <li>React</li>
+                    <li>WordPress</li>
+                </ul>
+            </Service>
+            <Service
+                icon="fas fa-mobile-alt"
+                title="Mobile Development"
+            >
+                <ul>
+                    <li>React Native</li>
+                    <li>iOS & Android</li>
+                    <li>Unity</li>
+                </ul>
+            </Service>
+            <Service
+                icon="fas fa-sitemap"
+                title="Operations & Strategy"
+            >
+                <ul>
+                    <li>Project Management</li>
+                    <li>Sysops & Consulting</li>
+                    <li>HIPAA & Security</li>
+                </ul>
+            </Service>
+        </Content>
     </Layout>
   )
 
