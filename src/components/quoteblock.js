ce60a1a832d3bcab3fdb887aba3f77bfc6710290
Marni Cohen <marni.a.cohen@gmail.com>
2018-10-08 11:58:32 -0700

    Basic blockquote in, non rotating

--- src/components/quoteblock.js
+++ src/components/quoteblock.js
@@ -4,13 +4,12 @@ import quoteblockStyles from "../styles/components/quoteblock.module.scss"
 export default props => (
     <div className={quoteblockStyles.testimonial}>  
         <h2>What People Say</h2>
-
-    <blockquote className={quoteblockStyles.testimonial__quote}>
-        Apsis has been exceptional to work with. More than just a vendor, Apsis is a partner whom we trust to interact directly with clients. Their work has consistently exceeded expectations, delivered on-time and on-budget. Developers who can thoughtfully problem solve and think critically are surprisingly rare and I look forward to working with them more in the future.
-    </blockquote>
-    <cite class="testimonial__attribution">
-        <span class="testimonial__name">Jo Fobo</span>
-        <span class="testimonial__company">CEO @ Frogs Inc</span>
-    </cite>
+        <blockquote className={quoteblockStyles.testimonial__quote}>
+            Apsis has been exceptional to work with. More than just a vendor, Apsis is a partner whom we trust to interact directly with clients. Their work has consistently exceeded expectations, delivered on-time and on-budget. Developers who can thoughtfully problem solve and think critically are surprisingly rare and I look forward to working with them more in the future.
+        </blockquote>
+        <cite>
+            <span className={quoteblockStyles.testimonial__name}>Jo Fobo</span>
+            <span className={quoteblockStyles.testimonial__company}>CEO @ Frogs Inc</span>
+        </cite>
     </div>
 )
