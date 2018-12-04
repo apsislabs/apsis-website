589d0071604c83f70f498255d4df869f2e82c4d9
Marni Cohen <marni.a.cohen@gmail.com>
2018-12-04 13:43:41 -0800

    add new blog post, minor style fixes on blog

--- src/components/author_block.js
+++ src/components/author_block.js
@@ -1,7 +1,7 @@
 import React from "react"
 import authorStyles from "../styles/components/author.module.scss"
 import { StaticQuery, graphql } from "gatsby"
-let author
+
 export default props => (
     <StaticQuery
         query={graphql`
@@ -19,18 +19,13 @@ export default props => (
         `}
         render={data => (
             <aside className={authorStyles.bio}>
-                {
-                    author = data.allPeopleYaml.edges.filter(o => o.node.name.includes(props.name))
-                    // console.log("IMG", author.image)
-                }
-                <img src={props.image} alt={author.name} className={authorStyles.bio__image}/>
-                {console.log("NODE", data.allPeopleYaml.edges.filter(o => o.node.name.includes(props.name)))}
+                <img src={data.allPeopleYaml.edges.filter(o => o.node.name.includes(props.name))[0].node.image} alt="Author Profile" className={authorStyles.bio__image}/>
                 <header className={authorStyles.bio__header}>
-                    <h5 className={authorStyles.bio__title}>{ props.authorName }</h5>
+                    <h5 className={authorStyles.bio__title}>{ data.allPeopleYaml.edges.filter(o => o.node.name.includes(props.name))[0].node.name }</h5>
                 </header>
 
                 <div className={authorStyles.bio__content}>
-                    {props.bio}
+                    {data.allPeopleYaml.edges.filter(o => o.node.name.includes(props.name))[0].node.bio}
                 </div>
             </aside>
         )}
