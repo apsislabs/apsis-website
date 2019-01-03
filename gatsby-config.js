7b671f7d89135536f3245327f7d04cf03be46bd6
Wyatt Kirby <wyatt@apsis.io>
2019-01-03 15:33:52 -0800

    Remove unused dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,64 +1,58 @@
 module.exports = {
-    pathPrefix: `/`,
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-catch-links`,
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        `gatsby-link`,
-        `gatsby-transformer-yaml`,
-        {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-            name: `data`,
-            path: `${__dirname}/src/data/`,
-          },
-        },
-        {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-            name: `src`,
-            path: `${__dirname}/src`,
-          },
-        },
-        {
-          resolve: 'gatsby-transformer-remark',
-          options: {
-            plugins: [
-              {
-                resolve: 'gatsby-remark-images',
-                options: {
-                  quality: 90,
-                },
-              },
-            ],
-          },
-        },
-        {
-          resolve: `gatsby-plugin-typography`,
+  pathPrefix: `/`,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-link`,
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`
+      }
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
             options: {
-              pathToConfigModule: `src/utils/typography.js`,
-            },
-        },
-        {
-          resolve: `gatsby-plugin-nprogress`,
-          options: {
-            // Setting a color is optional.
-            color: `#2773a7`,
-            // Disable the loading spinner.
-            showSpinner: false,
-          },
-        },
-        {
-          resolve: `gatsby-plugin-manifest`,
-          options: {
-            name: "Apsis Labs",
-            short_name: "Apsis",
-            start_url: "/",
-            icon: "src/images/favicons/favicon-16x16.png",
+              quality: 90
+            }
           }
-        },
-        'gatsby-plugin-offline'
-    ],
-}
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `#2773a7`,
+        // Disable the loading spinner.
+        showSpinner: false
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Apsis Labs",
+        short_name: "Apsis",
+        start_url: "/",
+        icon: "src/images/favicons/favicon-16x16.png"
+      }
+    },
+    "gatsby-plugin-offline"
+  ]
+};
--- src/utils/helpers.js
+++
@@ -1,3 +0,0 @@
-export function cl(...classes) {
-  return classes.filter(x => x).join(" ");
-}
