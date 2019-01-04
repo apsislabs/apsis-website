06bc5bf8341b4c86ca8aa32be23e2f996dff47cb
Wyatt Kirby <wyatt@apsis.io>
2019-01-03 16:35:46 -0800

    Update quoteblock styles

--- src/components/quoteblock.js
+++ src/components/quoteblock.js
@@ -9,7 +9,11 @@ const Arrow = ({ dir, style, onClick }) => {
     `fa-angle-${dir}`,
     "fa-fw",
     "fa-2x",
-    quoteblockStyles.arrow
+    quoteblockStyles.arrow,
+    {
+      [quoteblockStyles.arrowLeft]: dir === "left",
+      [quoteblockStyles.arrowRight]: dir === "right"
+    }
   );
 
   return <div className={arrowClasses} style={style} onClick={onClick} />;
@@ -21,16 +25,16 @@ const PrevArrow = props => <Arrow dir="left" {...props} />;
 const Quote = ({ quote, ...props }) => {
   return (
     <div {...props}>
-      <blockquote className={quoteblockStyles.testimonial__quote}>
+      <blockquote className={quoteblockStyles.quoteblock__quote}>
         {quote.text}
       </blockquote>
 
       <cite>
-        <span className={quoteblockStyles.testimonial__attribution}>
+        <span className={quoteblockStyles.quoteblock__attribution}>
           {quote.attribution}
         </span>
 
-        <span className={quoteblockStyles.testimonial__title}>
+        <span className={quoteblockStyles.quoteblock__title}>
           {quote.title} @ {quote.company}
         </span>
       </cite>
@@ -39,67 +43,27 @@ const Quote = ({ quote, ...props }) => {
 };
 
 class QuoteBlock extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      quotes: [
-        {
-          text:
-            "Apsis has been exceptional to work with. More than just a vendor, Apsis is a partner whom we trust to interact directly with clients. Their work has consistently exceeded expectations, delivered on-time and on-budget. Developers who can thoughtfully problem solve and think critically are surprisingly rare and I look forward to working with them more in the future.",
-          attribution: "Chris Goddard",
-          title: "Vice President of Analytics",
-          company: "Weber Shandwick"
-        },
-        {
-          text:
-            "Apsis Labs is an innovative and professional software development team that has provided Natera with valuable development services. The team has been instrumental in developing our customer facing and back office solutions. We are very happy with our decision to use Apsis, as their expertise has translated into back office cost savings and a better customer experience! Apsis is a great team to work with!",
-          attribution: "Susan Lin",
-          title: "Former Senior Manager, Enterprise Applications",
-          company: "Natera, Inc."
-        },
-        {
-          text:
-            "We have worked with Apsis on many projects ranging from website development, PHP application development, and Windows app development. Apsis has always delivered quality work on time, which is why we keep coming back to them for additional projects.",
-          attribution: "Omer Saeed",
-          title: "VP of Products, Marketing Automation",
-          company: "CallidusCloud"
-        }
-      ]
-    };
-  }
-
   render() {
+    const { quotes } = this.props;
+
     const settings = {
       dots: false,
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       nextArrow: <NextArrow />,
-      prevArrow: <PrevArrow />,
-      responsive: [
-        {
-          breakpoint: 375,
-          settings: {
-            arrows: false,
-            swipe: true,
-            swipeToSlide: true,
-            adaptiveHeight: true,
-            dots: true
-          }
-        }
-      ]
+      prevArrow: <PrevArrow />
     };
 
     return (
       <div className={quoteblockStyles.quoteblock}>
-        <h3 className={quoteblockStyles.quoteblock__title}>
+        <h3 className={quoteblockStyles.quoteblock__header}>
           What Our Partners Say
         </h3>
 
         <div className={quoteblockStyles.quoteblock__container}>
           <Slider {...settings}>
-            {this.state.quotes.map(function(quote, i) {
+            {quotes.map(function(quote, i) {
               return <Quote key={i} quote={quote} />;
             })}
           </Slider>
--- src/pages/services.js
+++ src/pages/services.js
@@ -18,7 +18,9 @@ const ServicesHero = ({ data }) => (
   <Hero displayname="Hero" imgSrc={data.hero.childImageSharp.fluid.src}>
     <Animated animationIn="fadeInUp">
       <h1>Simply Software.</h1>
-      <h3>They say: do one thing and do it well. Our thing is software.</h3>
+      <p className="lede">
+        They say: do one thing and do it well. Our thing is software.
+      </p>
     </Animated>
 
     <Button
@@ -33,21 +35,6 @@ const ServicesHero = ({ data }) => (
     </Button>
   </Hero>
 );
-// toolOne={data.toolUnity.childImageSharp.fluid}
-//           toolOneAlt="Unity"
-//           toolOneUrl="https://www.webershandwick.com/"
-//           toolTwo={data.toolWP.childImageSharp.fluid}
-//           toolTwoAlt="Wordpress"
-//           toolTwoUrl="https://www.natera.com/"
-//           toolThree={data.toolAWS.childImageSharp.fluid}
-//           toolThreeAlt="AWS"
-//           toolThreeUrl="https://tray.io/"
-//           toolFour={data.toolReact.childImageSharp.fluid}
-//           toolFourAlt="React"
-//           toolFourUrl="https://www.boku.com/"
-//           toolFive={data.toolRails.childImageSharp.fluid}
-//           toolFiveAlt="Ruby on Rails"
-//           toolFiveUrl="https://www.boku.com/"
 
 const ServicesPage = data => {
   const { toolUnity, toolWP, toolAWS, toolReact, toolRails } = data;
@@ -78,6 +65,31 @@ const ServicesPage = data => {
       alt: "Ruby on Rails"
     }
   ];
+
+  const quotes = [
+    {
+      text:
+        "Apsis has been exceptional to work with. More than just a vendor, Apsis is a partner whom we trust to interact directly with clients. Their work has consistently exceeded expectations, delivered on-time and on-budget. Developers who can thoughtfully problem solve and think critically are surprisingly rare and I look forward to working with them more in the future.",
+      attribution: "Chris Goddard",
+      title: "Vice President of Analytics",
+      company: "Weber Shandwick"
+    },
+    {
+      text:
+        "Apsis Labs is an innovative and professional software development team that has provided Natera with valuable development services. The team has been instrumental in developing our customer facing and back office solutions. We are very happy with our decision to use Apsis, as their expertise has translated into back office cost savings and a better customer experience! Apsis is a great team to work with!",
+      attribution: "Susan Lin",
+      title: "Former Senior Manager, Enterprise Applications",
+      company: "Natera, Inc."
+    },
+    {
+      text:
+        "We have worked with Apsis on many projects ranging from website development, PHP application development, and Windows app development. Apsis has always delivered quality work on time, which is why we keep coming back to them for additional projects.",
+      attribution: "Omer Saeed",
+      title: "VP of Products, Marketing Automation",
+      company: "CallidusCloud"
+    }
+  ];
+
   return (
     <Layout>
       <Helmet title="Services | Apsis Labs" />
@@ -100,9 +112,9 @@ const ServicesPage = data => {
         </p>
       </ContentBlock>
 
-      <QuoteBlock />
-
+      <QuoteBlock quotes={quotes} />
       <ImageGrid images={toolImages} />
+
       <HowWeWork />
 
       <HireUsFooter />
